1630d5499eeae42e500bad7205b9d53c8079ab94
mikamism <mikamism666@gmail.com>
2017-04-19 17:19:39 +0900

    add confirm

--- index.php
+++ index.php
@@ -93,7 +93,6 @@ foreach ($events as $event) {
   replyConfirmTemplate($bot,
     $event->getReplyToken(),
     "Webで詳しく見ますか？",
-    "Webで詳しく見ますか？",
     new LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder (
       "見る", "http://google.jp"),
     new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder (
